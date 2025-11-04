ed1a9222e7065e50ddd89bbfc36eed0c3d0f76e8
Giovanni <gbaraldi@amd.com>
2025-11-04 16:35:55 -0600

    Format error messages

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/core.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/core.cpp
@@ -319,7 +319,7 @@ ThreadTracerAgent::stop_thread_trace()
     }
     else
     {
-        auto control_packet = get_control(true);
+        auto control_packet = get_control(false);
         control_packet->clear();
         // Join helpers and emit the final set of packets so the GPU drains.
         control_packet->populate_after();
