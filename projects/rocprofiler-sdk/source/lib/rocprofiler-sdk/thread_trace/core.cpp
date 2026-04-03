c0c4c100e6e3cdad277785e82d0a51ac080121ee
Venkateshwar Reddy Kandula <venkateshwar.kandula1306@gmail.com>
2026-04-03 00:06:31 -0500

    typo.

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/core.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/core.cpp
@@ -529,7 +529,7 @@ DeviceThreadTracer::resource_init()
             continue;
         }
 
-        agents[it->first] = std::make_unique<ThreadTracerQueue>(it->second, rocp_agent->id);
+        agents[it->first] = std::make_unique<ThreadTracerAgent>(it->second, rocp_agent->id);
     }
 }
 
