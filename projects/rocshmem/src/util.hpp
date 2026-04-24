048f37e3b2c8e33e13cb248fb61efeab60b5346d
amirsojoodi <amir.sojoodi@amd.com>
2026-04-24 16:30:06 +0000

    Fix: direct size 1 to store_asm

--- projects/rocshmem/src/util.hpp
+++ projects/rocshmem/src/util.hpp
@@ -385,17 +385,13 @@ __device__ __forceinline__ bool is_last_active_lane() {
   uint8_t* dst_bytes{static_cast<uint8_t*>(dst)};
   uint8_t* src_bytes{static_cast<uint8_t*>(src)};
 
-  for (size_t i = 16; i > 1; i >>= 1) {
+  for (size_t i = 16; i >= 1; i >>= 1) {
     while (size >= i) {
       store_asm(src_bytes, dst_bytes, i);
       src_bytes += i;
       dst_bytes += i;
       size -= i;
     }
-  }
-
-  if (size == 1) {
-    *dst_bytes = *src_bytes;
   }
 }
 
@@ -414,7 +410,7 @@ __device__ __forceinline__ bool is_last_active_lane() {
   dst_bytes = dst_def;
   src_bytes = src_def;
 
-  for (int j = 16; j > 1; j >>= 1) {
+  for (int j = 16; j >= 1; j >>= 1) {
     cpy_size = size / j;
     for (int i = thread_id; i < cpy_size; i += block_size) {
       dst_bytes = dst_def;
@@ -428,12 +424,6 @@ __device__ __forceinline__ bool is_last_active_lane() {
     size -= cpy_size * j;
     dst_def += cpy_size * j;
     src_def += cpy_size * j;
-  }
-
-  if (size == 1) {
-    if (is_thread_zero_in_block()) {
-      *dst_bytes = *src_bytes;
-    }
   }
 }
 
@@ -452,7 +442,7 @@ __device__ __forceinline__ bool is_last_active_lane() {
   dst_bytes = dst_def;
   src_bytes = src_def;
 
-  for (int j = 16; j > 1; j >>= 1) {
+  for (int j = 16; j >= 1; j >>= 1) {
     cpy_size = size / j;
     for (int i = wave_tid; i < cpy_size; i += wave_size) {
       dst_bytes = dst_def;
@@ -466,12 +456,6 @@ __device__ __forceinline__ bool is_last_active_lane() {
     size -= cpy_size * j;
     dst_def += cpy_size * j;
     src_def += cpy_size * j;
-  }
-
-  if (size == 1) {
-    if (is_thread_zero_in_wave()) {
-      *dst_bytes = *src_bytes;
-    }
   }
 }
 
