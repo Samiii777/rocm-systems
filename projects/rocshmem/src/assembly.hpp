b3bc606296eb716b0eba618374ae08fa664cf59e
amirsojoodi <amir.sojoodi@amd.com>
2026-04-24 16:29:45 +0000

    Dev: add case 1 to store_asm, fix case 2

--- projects/rocshmem/src/assembly.hpp
+++ projects/rocshmem/src/assembly.hpp
@@ -200,6 +200,25 @@ __device__ __forceinline__ void __roc_flush() {
 __device__ __forceinline__ void store_asm(uint8_t* val, [[maybe_unused]] uint8_t* dst,
                                           int size) {
   switch (size) {
+    case 1: {
+#if defined(__gfx90a__)
+      int16_t val16{static_cast<int16_t>(*val)};
+      asm volatile("flat_store_byte %0 %1 glc slc" : : "v"(dst), "v"(val16));
+#endif
+#if defined(__gfx942__) || defined(__gfx950__)
+      int16_t val16{static_cast<int16_t>(*val)};
+      asm volatile("flat_store_byte %0 %1 sc0 sc1" : : "v"(dst), "v"(val16));
+#endif
+#if defined(__gfx1100__)
+      int32_t val32{static_cast<int32_t>(*val)};
+      asm volatile("flat_store_byte %0 %1 glc slc" : : "v"(dst), "v"(val32));
+#endif
+#if defined(__gfx1201__)
+      int32_t val32{static_cast<int32_t>(*val)};
+      asm volatile("flat_store_b8 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val32));
+#endif
+      break;
+    }
     case 2: {
 #if defined(__gfx90a__)
       int16_t val16{*(reinterpret_cast<int16_t*>(val))};
@@ -210,11 +229,11 @@ __device__ __forceinline__ void store_asm(uint8_t* val, [[maybe_unused]] uint8_t
       asm volatile("flat_store_short %0 %1 sc0 sc1" : : "v"(dst), "v"(val16));
 #endif
 #if defined(__gfx1100__)
-      int32_t val32{*(reinterpret_cast<int32_t*>(val))};
+      int32_t val32{static_cast<int32_t>(*(reinterpret_cast<int16_t*>(val)))};
       asm volatile("flat_store_short %0 %1 glc slc" : : "v"(dst), "v"(val32));
 #endif
 #if defined(__gfx1201__)
-      int32_t val32{*(reinterpret_cast<int32_t*>(val))};
+      int32_t val32{static_cast<int32_t>(*(reinterpret_cast<int16_t*>(val)))};
       asm volatile("flat_store_b16 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val32));
 #endif
       break;
